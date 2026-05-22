7d593a34df0d613e7c0b02ffcf3c6542ea311c01
Potatoe415 <remi.in.sf@gmail.com>
2026-05-22 19:34:41 +0200

    Refactor: Flatten engines structure into a single folder for better visibility

--- api/engines/index.js
+++ api/engines/index.js
@@ -1,14 +1,14 @@
-import apibay from './apibay/config.json' assert { type: 'json' };
-import limetorrents from './limetorrents/config.json' assert { type: 'json' };
-import yts from './yts/config.json' assert { type: 'json' };
-import solid from './solid/config.json' assert { type: 'json' };
-import nyaa from './nyaa/config.json' assert { type: 'json' };
-import x1337 from './1337x/config.json' assert { type: 'json' };
-import torrentz2 from './torrentz2/config.json' assert { type: 'json' };
-import kickass from './kickass/config.json' assert { type: 'json' };
-import fitgirl from './fitgirl/config.json' assert { type: 'json' };
-import torrent9 from './torrent9/config.json' assert { type: 'json' };
-import oxtorrent from './oxtorrent/config.json' assert { type: 'json' };
+import apibay from './apibay.json' assert { type: 'json' };
+import limetorrents from './limetorrents.json' assert { type: 'json' };
+import yts from './yts.json' assert { type: 'json' };
+import solid from './solid.json' assert { type: 'json' };
+import nyaa from './nyaa.json' assert { type: 'json' };
+import x1337 from './1337x.json' assert { type: 'json' };
+import torrentz2 from './torrentz2.json' assert { type: 'json' };
+import kickass from './kickass.json' assert { type: 'json' };
+import fitgirl from './fitgirl.json' assert { type: 'json' };
+import torrent9 from './torrent9.json' assert { type: 'json' };
+import oxtorrent from './oxtorrent.json' assert { type: 'json' };
 
 export const ENGINE_CONFIGS = {
   apibay,
